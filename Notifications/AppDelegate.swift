fd066902c98bdd167389998f22d597af0e4ff9b1
Oleg Kanatov <olegkanatov7@gmail.com>
2021-10-27 13:32:34 +0300

    add Local Notification with Actions

--- Notifications/AppDelegate.swift
+++ Notifications/AppDelegate.swift
@@ -54,11 +54,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func scheduleNotification(notificationType: String) {
         
         let content = UNMutableNotificationContent()
+        let userAction = "User Action"
         
         content.title = notificationType
         content.body = "This is example how to create " + notificationType
         content.sound = UNNotificationSound.default
         content.badge = 1
+        content.categoryIdentifier = userAction
         
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
         
@@ -72,9 +74,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 print("Error: \(error.localizedDescription)")
             }
         }
+        
+        let snoozeAction = UNNotificationAction(identifier: "Snooze", title: "Snooze", options: [])
+        let deleteAction = UNNotificationAction(identifier: "Delete", title: "Delete", options: [.destructive])
+        let category = UNNotificationCategory(identifier: userAction,
+                                              actions: [snoozeAction, deleteAction],
+                                              intentIdentifiers: [],
+                                              options: [])
+        
+        notificationCenter.setNotificationCategories([category])
     }
-
-
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
@@ -88,6 +97,20 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         
         if response.notification.request.identifier == "Local Notification" {
             print("Handling notification with the Local Notification Identifire")
+        }
+        
+        switch response.actionIdentifier {
+        case UNNotificationDismissActionIdentifier:
+            print("Dismiss Action")
+        case UNNotificationDefaultActionIdentifier:
+            print("Default")
+        case "Snooze":
+            print("Snooze")
+            scheduleNotification(notificationType: "Reminder")
+        case "Delete":
+            print("Delete")
+        default:
+            print("Unknown action")
         }
         
         completionHandler()
